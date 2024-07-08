3ef6d1e9985988c03ac81434dd6b4259769c9f30
KevinPico04 <118645617+KevinPico04@users.noreply.github.com>
2024-07-07 23:28:16 -0500

    Cambios

--- KEVIN DAVID PICO REYES/WebApplication1/Program.cs
+++ KEVIN DAVID PICO REYES/WebApplication1/Program.cs
@@ -1,11 +1,4 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Microsoft.AspNetCore.Authorization;
 using WebApplication1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,49 +9,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-// Configuración de la autenticación JWT
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        var jwtKey = builder.Configuration["Jwt:Key"];
-        if (jwtKey == null)
-        {
-            throw new InvalidOperationException("JWT key is not configured.");
-        }
 
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
-        };
-
-        // Configura el desafío de autenticación
-        options.Events = new JwtBearerEvents
-        {
-            OnChallenge = context =>
-            {
-                // Configura la respuesta con WWW-Authenticate header
-                context.HandleResponse();
-                context.Response.Headers["WWW-Authenticate"] = "Bearer";
-                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                return Task.CompletedTask;
-            }
-        };
-    });
-
-// Configura el JWT en los servicios
-builder.Services.AddAuthorization(options =>
-{
-    options.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
-        .RequireAuthenticatedUser()
-        .Build();
-});
 
 var app = builder.Build();
 
@@ -71,8 +25,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// Añadir middleware de autenticación y autorización
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
