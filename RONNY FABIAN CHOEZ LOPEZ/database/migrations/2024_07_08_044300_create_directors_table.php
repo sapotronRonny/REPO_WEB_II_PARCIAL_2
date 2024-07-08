3b189eacace39fc6a8042c19c9cae4a6ece1c7c9
sapotronRonny <ronnychoez23@gmail.com>
2024-07-07 23:55:30 -0500

    update

--- RONNY FABIAN CHOEZ LOPEZ/database/migrations/2024_07_08_044300_create_directors_table.php
+++ RONNY FABIAN CHOEZ LOPEZ/database/migrations/2024_07_08_044300_create_directors_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('directors', function (Blueprint $table) {
-            $table->id();
+            $table->id('director_id');
+            $table->string('nombre', 100);
+            $table->string('pais_origen', 100)->nullable();
+            $table->date('fecha_nacimiento')->nullable();
             $table->timestamps();
         });
     }
